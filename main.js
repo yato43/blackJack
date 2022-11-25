3aa57ce3ec6872aeb2e8571391ccf63e24888aea
Yato <akochenokvm@gmail.com>
2022-11-25 22:10:14 +0300

    1

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-let firstCard = 9
-let secondCard = 6
+let firstCard = 10
+let secondCard = 11
 
 
 let sum = firstCard + secondCard
+let hasBlackJack = false
 
-console.log(sum)
+
+if (sum<21){
+    console.log('Do you want tot draw a new card?')
+}else if (sum === 21){
+    console.log("Wohoo! you've got BlackJAck!")
+}else if (sum > 21){
+    console.log("you're not out of the game! 😭")
+}

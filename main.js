40ce999d4f7127c838e0fc7ab449dd2d75be4c74
Yato <akochenokvm@gmail.com>
2022-11-29 22:23:37 +0300

    some changes

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
 let firstCard = 10
-let secondCard = 12
+let secondCard = 10
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isALive = true
 let message = ""
+let messageEl = document.getElementById('message-el')
+let sumEl = document.getElementById('sum-el')
+let cardsEl = document.getElementById('cards-el')
 
 function startGame() {
-    if (sum<21){
-        message = ('Do you want tot draw a new card?')
-    }else if (sum === 21){
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum;
+    if (sum < 21) {
+        message = ('Do you want to draw a new card?')
+    } else if (sum === 21) {
         message = ("Wohoo! you've got BlackJAck!")
         hasBlackJack = true
-    }else if (sum > 21){
-        message = ("you're not out of the game! 😭")
+    } else if (sum > 21) {
+        message = ("you're out of the game! ")
         isALive = false
     }
-    console.log(message)
+    messageEl.textContent = message
 }
 
 
